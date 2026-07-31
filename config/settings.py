47e1444ebc5697d360183847f7c62b7387917968
Ilya <ferydeer2098@gmail.com>
2026-07-31 23:29:16 +0500

    Add gunicorn

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 import dj_database_url
 
 load_dotenv()
+print("REDIS_URL =", os.getenv("REDIS_URL"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
